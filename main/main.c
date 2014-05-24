855de463f74537c48963a345d3891c87acb85442
jandadav <janda.david@gmail.com>
2014-05-24 11:05:15 +0200

    redefined trims 3
    trims redefined again

--- main/main.c
+++ main/main.c
@@ -445,8 +445,8 @@ int __attribute__((noreturn)) main(void)
 				*/
 				if (bit_is_set(device_configuration, 6))
 				{
-					a2dData[0] -= (0.25*(a2dData[3]-2048));
-					a2dData[1] -= (1.2*(a2dData[4]-2048));
+					a2dData[0] -= (0.35*(a2dData[3]-2048));
+					a2dData[1] -= (0.9*(a2dData[4]-2048));
 					a2dData[2] -= (0.25*(a2dData[5]-2048));
 				}else{
 					a2dData[0] -= (0.5*(a2dData[3]-2048));
